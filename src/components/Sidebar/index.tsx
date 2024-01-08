9ddd93ace77fa9f4e7f5faaf557027bbadcc627a
Lucas Rocha Fernandes <lucasrochaf2018@gmail.com>
2024-01-08 20:26:44 -0300

    Nav

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,19 +1,21 @@
 import { Search } from 'lucide-react'
 
 import { Logo } from './Logo'
+import { MainNavigation } from './MainNavigation'
 
 export function Sidebar() {
   return (
     <aside className="border-sinc-200 space-y-6 border-r px-5 py-8">
       <Logo />
-      <div className="boder-sinc-300 flex w-full items-center gap-2 rounded-lg border px-3 py-2 shadow-sm">
+      <div className="boder-sinc-300 mx-1 flex w-full items-center gap-2 rounded-lg border px-3 py-2 shadow-sm">
         <Search className="h-5 w-5 text-zinc-500" />
         <input
-          className="placeholder:zinc-600 flex-1 border-0 bg-transparent p-0 text-zinc-900"
+          className="placeholder:zinc-600 flex-1 border-0 bg-transparent p-0 text-zinc-900 focus:outline-none"
           type="text"
           placeholder="Search"
         />
       </div>
+      <MainNavigation />
     </aside>
   )
 }
