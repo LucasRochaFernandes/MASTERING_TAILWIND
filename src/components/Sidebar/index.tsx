544f29e996051f139213a1d2ec69643d2de3319b
Lucas Rocha Fernandes <lucasrochaf2018@gmail.com>
2024-01-09 22:53:56 -0300

    Patterns

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -9,6 +9,7 @@ import {
   Users,
 } from 'lucide-react'
 
+import { InputControl, InputPrefix, InputRoot } from '../input'
 import { Logo } from './Logo'
 import { NavItem } from './NavItem'
 import { Profile } from './Profile'
@@ -18,14 +19,12 @@ export function Sidebar() {
   return (
     <aside className="border-sinc-200 flex flex-col gap-6 border-r px-5 py-8">
       <Logo />
-      <div className="boder-sinc-300 mx-1 flex w-full items-center gap-2 rounded-lg border px-3 py-2 shadow-sm">
-        <Search className="h-5 w-5 text-zinc-500" />
-        <input
-          className="placeholder:zinc-600 flex-1 border-0 bg-transparent p-0 text-zinc-900 focus:outline-none"
-          type="text"
-          placeholder="Search"
-        />
-      </div>
+      <InputRoot>
+        <InputPrefix>
+          <Search className="h-5 w-5 text-zinc-500" />
+        </InputPrefix>
+        <InputControl className="Search" type="text" />
+      </InputRoot>
       <nav className="space-y-0.5">
         <NavItem title={'Home'} icon={Home} />
         <NavItem title={'Dashboard'} icon={BarChart} />
